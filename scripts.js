e05d39a9e447be72e2eabb1fd662c820b6f1a6d8
mimocha.pc <l.chawit@outlook.com>
2019-03-03 03:29:43 +0900

    Orientation Change Function

--- scripts.js
+++ scripts.js
@@ -1,26 +1,51 @@
 var state = 0;
 
+// Orientation Check
+function orientation () {
+	var img_0 = document.getElementById('img-0');
+	var vid_0 = document.getElementById('vid-0');
+	var vid_1 = document.getElementById('vid-1');
+	var vid_2 = document.getElementById('vid-2');
+
+	if(window.innerHeight > window.innerWidth){
+		img_0.style.objectFit = "contain";
+		vid_0.style.objectFit = "contain";
+		vid_1.style.objectFit = "contain";
+		vid_2.style.objectFit = "contain";
+	} else {
+		img_0.style.objectFit = "cover";
+		vid_0.style.objectFit = "cover";
+		vid_1.style.objectFit = "cover";
+		vid_2.style.objectFit = "cover";
+	}
+}
+
 // Finite State Machine
 function fsm () {
+	var img_0 = document.getElementById('img-0');
+	var vid_0 = document.getElementById('vid-0');
+	var vid_1 = document.getElementById('vid-1');
+	var vid_2 = document.getElementById('vid-2');
+
 	switch (state) {
 		case 0: // First Right -> Left
-			document.getElementById('img-0').style.visibility = "hidden";
-			document.getElementById('vid-0').style.visibility = "visible";
-			document.getElementById('vid-0').play();
+			img_0.style.visibility = "hidden";
+			vid_0.style.visibility = "visible";
+			vid_0.play();
 			state = 1;
 			break;
 		case 1: // Left -> Right
-			document.getElementById('vid-1').style.visibility = "visible";
-			document.getElementById('vid-2').style.visibility = "hidden";
-			document.getElementById('vid-2').currentTime = 0;
-			document.getElementById('vid-1').play();
+			vid_1.style.visibility = "visible";
+			vid_2.style.visibility = "hidden";
+			vid_2.currentTime = 0;
+			vid_1.play();
 			state = 2;
 			break;
 		case 2: // Right -> Left
-			document.getElementById('vid-1').style.visibility = "hidden";
-			document.getElementById('vid-1').currentTime = 0;
-			document.getElementById('vid-2').style.visibility = "visible";
-			document.getElementById('vid-2').play();
+			vid_1.style.visibility = "hidden";
+			vid_1.currentTime = 0;
+			vid_2.style.visibility = "visible";
+			vid_2.play();
 			state = 1;
 			break;
 	}
