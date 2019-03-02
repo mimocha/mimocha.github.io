9a24f3ee4cabcb33a4a5c264291be4ac6b14dce2
mimocha.pc <l.chawit@outlook.com>
2019-03-03 02:16:33 +0900

    MP4 Caching Hack with CSS Trickery

--- scripts.js
+++ scripts.js
@@ -4,20 +4,23 @@ var state = 0;
 function fsm () {
 	switch (state) {
 		case 0: // First Right -> Left
-			document.getElementById('vid-elem').style.visibility = "visible";
-			document.getElementById('vid-elem').play();
+			document.getElementById('img-0').style.visibility = "hidden";
+			document.getElementById('vid-0').style.visibility = "visible";
+			document.getElementById('vid-0').play();
 			state = 1;
 			break;
 		case 1: // Left -> Right
-			document.getElementById('vid-elem').src='vid/chika-1.mp4';
-			document.getElementById('img-elem').src='img/chika-1.jpg';
-			document.getElementById('vid-elem').play();
+			document.getElementById('vid-1').style.visibility = "visible";
+			document.getElementById('vid-2').style.visibility = "hidden";
+			document.getElementById('vid-2').currentTime = 0;
+			document.getElementById('vid-1').play();
 			state = 2;
 			break;
 		case 2: // Right -> Left
-			document.getElementById('vid-elem').src='vid/chika-2.mp4';
-			document.getElementById('img-elem').src='img/chika-2.jpg';
-			document.getElementById('vid-elem').play();
+			document.getElementById('vid-1').style.visibility = "hidden";
+			document.getElementById('vid-1').currentTime = 0;
+			document.getElementById('vid-2').style.visibility = "visible";
+			document.getElementById('vid-2').play();
 			state = 1;
 			break;
 	}
