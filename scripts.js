18fec6863df329d2508234c7bc812ec7b930fe59
mimocha <l.chawit@outlook.com>
2020-02-19 12:18:27 +0000

    Updated js functions Removed global vars, using dom instead

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 "use strict";
 
 // OnLoad Event Function
-var obj;
-var len;
 window.onload = function (e) {
-	obj = document.querySelectorAll(".media");
-	len = obj.length
 	orient ();
 }
 
@@ -18,7 +14,10 @@ function orient () {
 	var setval;
 	if (window.innerHeight > window.innerWidth) { setval = "contain"; }
 	else { setval = "cover"; }
-	for (var i = 0; i < len; i++) { obj[i].style.objectFit = setval; }
+
+	const obj = document.querySelectorAll(".media");
+	const len = obj.length;
+	for (let i = 0; i < len; i++) { obj[i].style.objectFit = setval; }
 }
 
 
@@ -36,42 +35,42 @@ fsm.val = 0;
 function fsm () {
 	switch (fsm.val) {
 		case 0: // First Right -> Left
-			obj[0].style.visibility = "hidden";
-			obj[1].style.visibility = "visible";
-			obj[1].play();
+			document.getElementById("img-0").style.visibility = "hidden";
+			document.getElementById("vid-0").style.visibility = "visible";
+			document.getElementById("vid-0").play();
 			fsm.val = 1;
 			break;
 
 		case 1: // Left -> Right
-			obj[1].style.visibility = "hidden";
-			obj[5].style.visibility = "hidden";
-			obj[5].currentTime = 0;
-			obj[2].style.visibility = "visible";
-			obj[2].play();
+			document.getElementById("vid-0").style.visibility = "hidden";
+			document.getElementById("vid-4").style.visibility = "hidden";
+			document.getElementById("vid-4").currentTime = 0;
+			document.getElementById("vid-1").style.visibility = "visible";
+			document.getElementById("vid-1").play();
 			fsm.val = 2;
 			break;
 
 		case 2: // Right -> Left
-			obj[2].style.visibility = "hidden";
-			obj[2].currentTime = 0;
-			obj[3].style.visibility = "visible";
-			obj[3].play();
+			document.getElementById("vid-1").style.visibility = "hidden";
+			document.getElementById("vid-1").currentTime = 0;
+			document.getElementById("vid-2").style.visibility = "visible";
+			document.getElementById("vid-2").play();
 			fsm.val = 3;
 			break;
 
 		case 3: // Left -> Right
-			obj[3].style.visibility = "hidden";
-			obj[3].currentTime = 0;
-			obj[4].style.visibility = "visible";
-			obj[4].play();
+			document.getElementById("vid-2").style.visibility = "hidden";
+			document.getElementById("vid-2").currentTime = 0;
+			document.getElementById("vid-3").style.visibility = "visible";
+			document.getElementById("vid-3").play();
 			fsm.val = 4;
 			break;
 
 		case 4: // Right -> Left
-			obj[4].style.visibility = "hidden";
-			obj[4].currentTime = 0;
-			obj[5].style.visibility = "visible";
-			obj[5].play();
+			document.getElementById("vid-3").style.visibility = "hidden";
+			document.getElementById("vid-3").currentTime = 0;
+			document.getElementById("vid-4").style.visibility = "visible";
+			document.getElementById("vid-4").play();
 			fsm.val = 1;
 			break;
 	}
