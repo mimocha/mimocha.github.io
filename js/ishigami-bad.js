101baeb76b079637d91a781722042d0bc622c7a8
mimocha <l.chawit@outlook.com>
2020-02-20 11:36:44 +0000

    Added favicons, chrome tab color, background color, and more audio. Renamed js folder & Tweaks to js. Problem with Edge media query not working properly.

---
+++ js/ishigami-bad.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// Click Event Function
+window.onclick = function clicked (e) {
+	console.log("count =", counter.val, "fsm = ", fsm.val, "sound = ", sound.val);
+	fsm ();
+	sound ();
+	counter ();
+}
+
+
+// Finite State Machine
+fsm.val = 0;
+function fsm () {
+	switch (fsm.val) {
+		case 0: // First Right -> Left
+		// document.getElementById("img-0").style.visibility = "hidden";
+		document.getElementById("vid-0").style.visibility = "visible";
+		document.getElementById("vid-0").play();
+		fsm.val = 1;
+		break;
+
+		case 1: // Left -> Right
+		document.getElementById("vid-0").style.visibility = "hidden";
+		document.getElementById("vid-1").style.visibility = "visible";
+		document.getElementById("vid-4").style.visibility = "hidden";
+		document.getElementById("vid-1").play();
+		document.getElementById("vid-4").currentTime = 0;
+		fsm.val = 2;
+		break;
+
+		case 2: // Right -> Left
+		document.getElementById("vid-2").style.visibility = "visible";
+		document.getElementById("vid-1").style.visibility = "hidden";
+		document.getElementById("vid-2").play();
+		document.getElementById("vid-1").currentTime = 0;
+		fsm.val = 3;
+		break;
+
+		case 3: // Left -> Right
+		document.getElementById("vid-3").style.visibility = "visible";
+		document.getElementById("vid-2").style.visibility = "hidden";
+		document.getElementById("vid-3").play();
+		document.getElementById("vid-2").currentTime = 0;
+		fsm.val = 4;
+		break;
+
+		case 4: // Right -> Left
+		document.getElementById("vid-4").style.visibility = "visible";
+		document.getElementById("vid-3").style.visibility = "hidden";
+		document.getElementById("vid-4").play();
+		document.getElementById("vid-3").currentTime = 0;
+		fsm.val = 1;
+		break;
+	}
+}
+
+
+// Play Sound
+sound.val = 0;
+function sound () {
+	switch (sound.val) {
+		case 0:
+			document.getElementById('aud-0').play();
+			break;
+		case 1:
+			document.getElementById('aud-1').play();
+			break;
+		case 2:
+			document.getElementById('aud-2').play();
+			break;
+		case 3:
+			document.getElementById('aud-3').play();
+			break;
+	}
+	sound.val += Math.floor(Math.random()*3);
+	sound.val %= 4;
+}
+
+
+// Click Counter
+counter.val = 0;
+function counter () {
+	counter.val++;
+}
