72636d59d40dfb55978f31354795b1ebba03d2ca
mimocha <l.chawit@outlook.com>
2020-04-02 19:23:06 +0100

    Neuomorphism UI design. Very pretty. Very Neutral.

--- js/index.js
+++ js/index.js
@@ -5,29 +5,35 @@ const btnFullview = document.getElementById("btn-fullview");
 btnNavbar.onclick = function () {
 	let panel = document.getElementsByClassName("outer")[0];
 	let btn = document.getElementById("btn-navbar");
+	let arrow = document.getElementById("btn-navbar").childNodes[1];
 
 	if (panel.style.left === "0rem") {
 		// Collapse
 		panel.style.left = "-20rem";
 		btn.style.left = "0rem";
+		arrow.style.rotate = "0deg";
 	} else {
 		// Expand
 		panel.style.left = "0rem";
 		btn.style.left = "20rem";
+		arrow.style.rotate = "180deg";
 	}
 }
 
 btnFullview.onclick = function () {
 	let panel = document.getElementsByClassName("outer")[2];
 	let btn = document.getElementById("btn-fullview");
+	let arrow = document.getElementById("btn-fullview").childNodes[1];
 
 	if (panel.style.right === "0rem") {
 		// Collapse
 		panel.style.right = "-45rem";
 		btn.style.right = "0rem";
+		arrow.style.rotate = "0deg";
 	} else {
 		// Expand
 		panel.style.right = "0rem";
 		btn.style.right = "45rem";
+		arrow.style.rotate = "180deg";
 	}
 }
