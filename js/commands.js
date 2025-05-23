a7d0cca9e19fc035090ba08782a53807b97e86c3
mimocha <chawit.leosrisook@outlook.com>
2025-05-23 18:09:17 +0700

    added command history, and dark-light themes

--- js/commands.js
+++ js/commands.js
@@ -1,11 +1,13 @@
 const banner = `<a href="#" data-command="banner">banner</a>`
 const clear = `<a href="#" data-command="clear">clear</a>`
 const contact = `<a href="#" data-command="contact">contact</a>`
+const dark = `<a href="#" data-command="dark">dark</a>`
 const echo = `<a href="#" data-command="echo">echo</a>`
 const email = `<a href="#" data-command="email">email</a>`
 const github = `<a href="#" data-command="github">github</a>`
 const help = `<a href="#" data-command="help">help</a>`
 const linkedin = `<a href="#" data-command="linkedin">linkedin</a>`
+const light = `<a href="#" data-command="light">light</a>`
 const welcome = `<a href="#" data-command="welcome">welcome</a>`
 const whoami = `<a href="#" data-command="whoami">whoami</a>`
 
@@ -14,42 +16,73 @@ const emailCopyLink = `<a href="#" onclick="copyEmail()">${emailAddress}</a>`;
 const githubLink = `<a href="https://github.com/mimocha" target="_blank" rel="noopener noreferrer">github/mimocha</a>`;
 const linkedinLink = `<a href="https://linkedin.com/in/chawitleosrisook" target="_blank" rel="noopener noreferrer">linkedin/chawitleosrisook</a>`;
 
-function copyEmail() {
-    navigator.clipboard.writeText(emailAddress);
-    displayOutput(`Email address copied to clipboard!`)
-}
-
 function getCommandPalette(outputDiv) {
     return {
         banner: () => {
-            return ` ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
- ░  ░░░░  ░░        ░░  ░░░░  ░░░      ░░░░      ░░░  ░░░░  ░░░      ░░
- ▒   ▒▒   ▒▒▒▒▒  ▒▒▒▒▒   ▒▒   ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒
- ▒        ▒▒▒▒▒  ▒▒▒▒▒        ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒        ▒▒  ▒▒▒▒  ▒
- ▓  ▓  ▓  ▓▓▓▓▓  ▓▓▓▓▓  ▓  ▓  ▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓  ▓▓        ▓
- ▓  ▓▓▓▓  ▓▓        ▓▓  ▓▓▓▓  ▓▓▓      ▓▓▓▓      ▓▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓  ▓
- ██████████████████████████████████████████████████████████████████████
-`;
+            return`<div class="yellow">
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░██░░░░██░░████████░░██░░░░██░░░██████░░░░██████░░░██░░░░██░░░██████░░
+░███░░███░░░░░██░░░░░███░░███░░██░░░░██░░██░░░░██░░██░░░░██░░██░░░░██░
+░████████░░░░░██░░░░░████████░░██░░░░██░░██░░░░░░░░████████░░██░░░░██░
+░██░██░██░░░░░██░░░░░██░██░██░░██░░░░██░░██░░░░██░░██░░░░██░░████████░
+░██░░░░██░░████████░░██░░░░██░░░██████░░░░██████░░░██░░░░██░░██░░░░██░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+</div>`;
+        },
+        clear: () => {
+            outputDiv.innerHTML = '';
+            return '';
+        },
+        contact: () => {
+            return `You can reach out to me at:
+Click to copy: ${emailCopyLink}
+${githubLink}
+${linkedinLink}`;
+        },
+        dark: () => {
+            bodyDiv.className = 'dark';
+            return `Dark mode selected!`;
+        },
+        echo: (args) => {
+            return args.join(' ');
+        },
+        email: () => {
+            return `Click to copy: ${emailCopyLink}`;
+        },
+        github: () => {
+            return githubLink;
+        },
+        help: () => {
+            return `Available terminal commands:
+    ${banner}    - Display the welcome banner
+    ${clear}     - Clear the terminal screen
+    ${contact}   - Show all my contact information
+    ${dark}      - Switch to dark mode
+    ${echo}      - Display a line of text (e.g., echo hello world)
+    ${email}     - Display my email address
+    ${github}    - Display my GitHub profile link
+    ${help}      - Display this help message
+    ${linkedin}  - Display my LinkedIn profile link
+    ${light}     - Switch to light mode
+    ${welcome}   - Display the welcome message again
+    ${whoami}    - Display information about me`;
+        },
+        hello: () => {
+            return `Hello! :)`;
+        },
+        linkedin: () => {
+            return linkedinLink;
+        },
+        light: () => {
+            bodyDiv.className = 'light';
+            return `Light mode selected!`;
         },
         welcome: () => {
             return `<b>Welcome to my github pages portfolio!</b>
 This is a terminal interface where you can learn more about me.
 Type <b>'help'</b> to see available commands
 Or click these links here for quick access:
-${help} ${whoami} ${contact}`;
-        },
-        help: () => {
-            return `Available terminal commands:
-  ${banner}    - Display the welcome banner
-  ${clear}     - Clear the terminal screen
-  ${contact}   - Show all my contact information
-  ${echo}      - Display a line of text (e.g., echo hello world)
-  ${email}     - Display my email address & copy to clipboard
-  ${github}    - Display my GitHub profile link
-  ${help}      - Display this help message
-  ${linkedin}  - Display my LinkedIn profile link
-  ${welcome}   - Display the welcome message again
-  ${whoami}    - Display information about me`;
+${help} ${whoami} ${email} ${github} ${linkedin}`;
         },
         whoami: () => {
             return `<b>Name: Chawit Leosrisook</b>
@@ -75,32 +108,11 @@ Also hobbyist programmer and AI researcher.
 <b>Education:</b>
 - MSc Artificial Intelligence graduate from the University of Sussex (United Kingdom)
 - BEng Robotics Engineering graduate from Nippon Institute of Technology (Japan)`;
-        },
-        contact: () => {
-            return `You can reach out to me at:
-${emailCopyLink}
-${githubLink}
-${linkedinLink}`;
-        },
-        email: () => {
-            copyEmail();
-            return emailCopyLink;
-        },
-        github: () => {
-            return githubLink;
-        },
-        linkedin: () => {
-            return linkedinLink;
-        },
-        clear: () => {
-            outputDiv.innerHTML = '';
-            return '';
-        },
-        echo: (args) => {
-            return args.join(' ');
-        },
-        hello: () => {
-            return `Hello! :)`;
         }
     };
+}
+
+function copyEmail() {
+    navigator.clipboard.writeText(emailAddress);
+    displayOutput(`Email address copied to clipboard!`)
 }
--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
+const bodyDiv = document.querySelector('body');
 const terminalDiv = document.getElementById('terminal');
 const outputDiv = document.getElementById('command-output');
 const commandInput = document.getElementById('command-input');
 const commandPrompt = document.getElementById('command-prompt');
-
 const commandPalette = getCommandPalette(outputDiv);
+const MAX_HISTORY_SIZE = 100;
 
-function displayOutput(text, className = 'normal') {
+function displayOutput(text, outputStatus = 'normal') {
     const line = document.createElement('div');
-    line.className = className;
+    line.className = outputStatus;
     line.innerHTML = text;
     outputDiv.appendChild(line);
 }
@@ -36,16 +37,25 @@ function processCommand(commandLine) {
 
     displayCommandEcho(commandLine);
 
+    // Output command result
     if (command in commandPalette) {
-        const output = commandPalette[command](args);
-        if (output) { // Some commands like 'clear' might not return output
-            displayOutput(output);
-        }
-    } else if (command === '') {
-        // No command entered, do nothing or display prompt again if needed
+        let output = commandPalette[command](args);
+        if (output) {displayOutput(output);}
+    } else if (command.length > 0) {
+        displayOutput(
+            `Command not found: ${command}. Type 'help' for a list of commands.`,
+            outputStatus = `error`);
     }
-    else {
-        displayOutput(`Command not found: ${command}. Type 'help' for a list of commands.`, 'error');
+
+    // Store command in history
+    if (command.length !== '') {
+        _ = commandHistory.unshift(commandLine);
+        // Remove the oldest command if over the max history size
+        if (commandHistory.length > MAX_HISTORY_SIZE) {
+            _ = commandHistory.pop();
+        }
+        console.log(commandHistory);
+        localStorage.setItem('commandHistory', JSON.stringify(commandHistory));
     }
 
     // Snap to the bottom of the output
@@ -53,33 +63,74 @@ function processCommand(commandLine) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Load locally stored command history
+    const storedHistory = localStorage.getItem('commandHistory')
+    if (storedHistory) {
+        commandHistory = JSON.parse(storedHistory);
+    } else {
+        commandHistory = [];
+    }
+    // Initialize command history index -- negative one represents current input
+    historyIndex = -1;
+    // Variable to store the current command being typed
+    pendingCommand = '';
+
     displayOutput(commandPalette.banner());
     displayOutput(commandPalette.welcome());
+});
 
-    commandInput.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Prevent default form submission
-            processCommand(commandInput.value);
-            commandInput.value = ''; // Clear input
+// Handles special key inputs (e.g., Enter, ArrowUp, ArrowDown)
+// Uses 'keydown' to have cursor move as expected
+commandInput.addEventListener('keydown', (event) => {
+    // Handle Enter key for command submission
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent default form submission
+        processCommand(commandInput.value);
+        commandInput.value = ''; // Clear input
+        pendingCommand = ''; // Reset current command
+    }
+    
+    // Handle ArrowUp and ArrowDown keys for command history navigation
+    else if ((event.key === 'ArrowUp') || (event.key === 'ArrowDown')) {
+        event.preventDefault(); // Prevent default scrolling
+
+        // Scroll up to earlier command, scroll down to more recent command
+        if ((event.key === 'ArrowUp') && (historyIndex < commandHistory.length - 1)) {
+            historyIndex++;
+        } else if ((event.key === 'ArrowDown') && (historyIndex > -1)) {
+            historyIndex--;
         }
-        // TODO: Add up-down command history navigation
-    });
 
-    // Handle clicks on terminal
-    terminalDiv.addEventListener('click', (event) => {
-        const target = event.target;
-
-        // Click anywhere in the terminal div to focus the command input
-        if (target !== commandInput) {
-            commandInput.focus();
+        // Negative one represents current input
+        if (historyIndex == -1) {
+            commandInput.value = pendingCommand;
+        } else {
+            commandInput.value = commandHistory[historyIndex];
         }
+    }
+});
 
-        // Check if the clicked element is a link with a data-command attribute
-        if (target.tagName === 'A' && target.dataset.command) {
-            event.preventDefault(); // Prevent default link navigation
-            const commandToRun = target.dataset.command;
-            processCommand(commandToRun);
-            commandInput.focus(); // Keep focus on the input
-        }
-    });
+// Handles all other "standard" key inputs (e.g., letters, numbers, symbols)
+// Uses 'keyup' to wait for charater to be registered first, otherwise the most recent character will be missing
+commandInput.addEventListener('keyup', (event) => {
+    if (event.key.length === 1) {
+        pendingCommand = commandInput.value;
+        historyIndex = -1; // Reset history index when typing a new command
+    }
+});
+
+// Handle clicks on terminal
+terminalDiv.addEventListener('click', (event) => {
+    // Click anywhere in the terminal div to focus the command input
+    if (event.target !== commandInput) {
+        commandInput.focus();
+    }
+
+    // Check if the clicked element is a link with a data-command attribute
+    if (event.target.tagName === 'A' && event.target.dataset.command) {
+        event.preventDefault(); // Prevent default link navigation
+        const commandToRun = event.target.dataset.command;
+        processCommand(commandToRun);
+        commandInput.focus(); // Keep focus on the input
+    }
 });
